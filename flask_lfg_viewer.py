a2ef20059a8c61c81930d624b72d8f5e32aece6e
Mark Williamson <markwilliamson@halifax.co.uk>
2025-08-06 20:00:34 +0100

    Create flask_lfg_viewer.py

---
+++ flask_lfg_viewer.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+from supabase import create_client
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+
+# --- LOAD ENV ---
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    systems = ["Any", "5E", "PF2E", "PATHFINDER", "CALL OF CTHULHU", "VTM"]
+    days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+    selected_system = request.args.get('system', 'Any')
+    selected_days = request.args.getlist('day')
+
+    query = supabase.table("posts").select("*")
+    if selected_system != "Any":
+        query = query.eq("system", selected_system)
+    for day in selected_days:
+        query = query.eq(day, 1)
+
+    results = query.execute().data
+
+    for post in results:
+        post['days'] = [d.capitalize() for d in days if post.get(d) == 1]
+        post['created'] = datetime.utcfromtimestamp(post['created_utc']).strftime('%Y-%m-%d %H:%M UTC')
+
+    return render_template("index.html", systems=systems, days=days,
+                           selected_system=selected_system,
+                           selected_days=selected_days,
+                           results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
